32525c6a2ba6a8bb10ec7dd3f9c157fd367f2f1b
Alexander Stratev <alexanderstratev@Alexanders-MacBook-Pro-2.local>
2021-05-31 14:43:04 -0500

    add facebook oauth

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ To download all the necessary dependencies use 'npm install'.
 
 [passport-google-oauth2.0](http://www.passportjs.org/packages/passport-google-oauth20/)
 
+
+
+# readme update...
+
+Added authentication with facebook using passport js.
+
+[passport-facebook](http://www.passportjs.org/packages/passport-facebook/)
+
+
 This a repo that will be used as a reference in future.
 
 P.S. Just learning to code...
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 
@@ -37,6 +38,7 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     googleId: String,
+    facebookId: String,
     secret: String
 });
 
@@ -57,11 +59,13 @@ passport.serializeUser(function(user, done) {
     });
   });
 
+
+  // authentication with google
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -69,6 +73,23 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+
+
+// authentication with facebook
+passport.use(new FacebookStrategy({
+    clientID: process.env.CLIENT_ID_FB,
+    clientSecret: process.env.CLIENT_SECRET_FB,
+    callbackURL: 'http://localhost:3000/auth/facebook/secrets',
+    profileFields: ['id', 'email', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get('/', function(req, res) {
     res.render('home');
@@ -85,6 +106,15 @@ app.get('/auth/google/secrets',
         }
 
 );
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  });
 
 
 app.get('/login', function(req, res) {
